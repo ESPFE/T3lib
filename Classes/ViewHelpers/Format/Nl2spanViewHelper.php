87ba08c58deac73014f52643b94c1da1b2e0d8ef
André Sünnemann <a.suennemann@edv-peuker.de>
2017-01-25 14:37:03 +0100

    added viewhelper Nl2spanViewHelper

---
+++ Classes/ViewHelpers/Format/Nl2spanViewHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace ESP\T3lib\ViewHelpers\Format;
+
+use TYPO3\CMS\Fluid\ViewHelpers\Format\Nl2brViewHelper;
+
+class Nl2spanViewHelper extends Nl2brViewHelper
+{
+    
+}
